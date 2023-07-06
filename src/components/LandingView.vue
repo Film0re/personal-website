574e10af9a7f24d10a90d670a1ccbc52901ddcc3
Daniel <dandoodle956@gmail.com>
2023-07-06 17:43:39 -0500

    Initial commit

---
+++ src/components/LandingView.vue
@@ -0,0 +1,20 @@
+<template>
+
+<div class="centered">
+    <h1>😎 Personal site in progress. Come back later 😎 </h1>
+</div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.centered{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+</style>
