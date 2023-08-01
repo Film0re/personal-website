393571abe01da806f48265117ddd5f5795160734
Daniel <dandoodle956@gmail.com>
2023-07-31 20:48:20 -0500

    Fixed img url pointing to non-existant img

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 import LandingView from './components/LandingView.vue';
+import workProject from './components/workProject.vue';
+import AboutMe from './components/AboutMe.vue';
 </script>
 
 <template>
   <LandingView />
+
 </template>
 
 <style scoped>
